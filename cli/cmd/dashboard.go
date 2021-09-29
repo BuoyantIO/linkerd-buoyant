abde5bcf1fd2ff17e3a899fba8806137675a5b29
Andrew Seigner <siggy@buoyant.io>
2021-09-29 10:25:25 -0700

    Modify `dashboard` cmd to handle browser failure (#33)
    If the `linkerd buoyant dashboard` command encountered a failure opening
    the default browser, it would just quit with an error.
    
    Modify `linkerd buoyant dashboard` to catch browser open errors, and
    instead print a message to the user instructing them to manually open a
    browser. This is similar to `linkerd viz dashboard` behavior.
    
    New output:
    ```
    $ go run cli/main.go dashboard
    Opening Buoyant Cloud dashboard in the default browser
    Failed to open dashboard automatically
    Visit https://buoyant.cloud in your browser to view the dashboard
    ```
    
    Fixes #32
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- cli/cmd/dashboard.go
+++ cli/cmd/dashboard.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +12,15 @@ func newCmdDashboard(cfg *config, openURL openURL) *cobra.Command {
 		Args:  cobra.NoArgs,
 		Short: "Open the Buoyant Cloud dashboard",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return openURL(cfg.bcloudServer)
+			fmt.Fprintln(cfg.stdout, "Opening Buoyant Cloud dashboard in the default browser")
+
+			err := openURL(cfg.bcloudServer)
+			if err != nil {
+				fmt.Fprintln(cfg.stderr, "Failed to open dashboard automatically")
+				fmt.Fprintf(cfg.stderr, "Visit %s in your browser to view the dashboard\n", cfg.bcloudServer)
+			}
+
+			return nil
 		},
 	}
 }
