4ae1a679d9092a9a86dd0599d442bec44863d9cc
Andrew Seigner <andrew@sig.gy>
2022-05-10 04:39:00 +0000

    Tombstone the repo
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- agent/pkg/api/manage_agent.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	pb "github.com/buoyantio/linkerd-buoyant/gen/bcloud"
-)
-
-func (c *Client) AgentCommands() <-chan *pb.AgentCommand {
-	return c.manageAgentStream.commands
-}
